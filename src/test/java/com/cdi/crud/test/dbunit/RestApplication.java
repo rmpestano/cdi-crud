252f6315ceb3876fff47c19d6989401346db9d54
rmpestano <rmpestano@gmail.com>
2014-12-26 23:30:05 -0200

    separated project into modules

--- src/test/java/com/cdi/crud/test/dbunit/RestApplication.java
+++
@@ -1,9 +0,0 @@
-package com.cdi.crud.test.dbunit;
-
-import javax.ws.rs.core.Application;
-import javax.ws.rs.ApplicationPath;
-
-@ApplicationPath("/rest")
-public class RestApplication extends Application
-{
-}
