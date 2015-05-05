188f47284e3a3b1969d53e7485e0a7e40b66e5c3
rmpestano <rmpestano@gmail.com>
2015-05-05 16:51:52 -0300

    throttling

--- src/test/scala/com/cdi/crud/perf/CdiCrudSimulation.scala
+++ src/test/scala/com/cdi/crud/perf/CdiCrudSimulation.scala
@@ -7,6 +7,8 @@ import scala.concurrent.duration._
 class CdiCrudSimulation extends Simulation {
 
 
+
+
   val httpProtocol = http
     .baseURL("http://localhost:8080/cdi-crud/")
     .acceptHeader("application/json;charset=utf-8")
@@ -21,10 +23,14 @@ class CdiCrudSimulation extends Simulation {
 
   setUp( //<4> scenario setup
     listCarsScenario.inject(
-      atOnceUsers(10), //<4>
-      rampUsersPerSec(1) to (10) during(20 seconds), //<5>
-      constantUsersPerSec(2) during (15 seconds))
+      //atOnceUsers(10), //<4>
+      //rampUsersPerSec(1) to (10) during(20 seconds), //<5>
+      constantUsersPerSec(10) during (10 minutes))
      )
+    .throttle(
+      reachRps(100) in (60 seconds),
+      holdFor(10 minute)
+    )
     .protocols(httpProtocol) //<6> request template
     .assertions(//<7>overall assertions
       global.successfulRequests.percent.greaterThan(95), //<8>for all requests
