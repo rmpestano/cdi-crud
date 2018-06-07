ff23d6a2ddc4e369f6fe0f2241e194c1f89722f5
rmpestano <rmpestano@gmail.com>
2018-06-07 19:30:02 -0300

    formating

--- src/test/java/com/cdi/crud/test/infra/CdiCrudAutomaticDeployment.java
+++ src/test/java/com/cdi/crud/test/infra/CdiCrudAutomaticDeployment.java
@@ -20,17 +20,17 @@ import org.jboss.shrinkwrap.resolver.api.maven.embedded.EmbeddedMaven;
  * @author rafael-pestano
  */
 public class CdiCrudAutomaticDeployment implements AutomaticDeployment {
-	
-	private static WebArchive deploymentCache; 
 
-    @Override
-    public DeploymentConfiguration generateDeploymentScenario(TestClass tc) {
-    	
-    	if(skipAutomaticDeployment(tc)) {
-    		return null; //skip if test class has @Deployment
-    	}
-    	
-    	if(deploymentCache == null) { //avoid rebuild project on every test class
+	private static WebArchive deploymentCache;
+
+	@Override
+	public DeploymentConfiguration generateDeploymentScenario(TestClass tc) {
+
+		if (skipAutomaticDeployment(tc)) {
+			return null; // skip if test class has @Deployment
+		}
+
+		if(deploymentCache == null) { //avoid rebuild project on every test class
     		deploymentCache = (WebArchive) EmbeddedMaven.forProject(new File("pom.xml"))
             .useMaven3Version("3.3.9")
             .setGoals("package")
@@ -39,12 +39,13 @@ public class CdiCrudAutomaticDeployment implements AutomaticDeployment {
             .ignoreFailure()
             .build().getDefaultBuiltArchive();
     	}
-        return new DeploymentContentBuilder(deploymentCache).get();
-       
-    }
+		
+		return new DeploymentContentBuilder(deploymentCache).get();
+
+	}
 
 	private boolean skipAutomaticDeployment(TestClass tc) {
 		return tc.getMethod(Deployment.class) != null;
 	}
-    
+
 }
