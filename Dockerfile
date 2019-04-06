aa4c16abb09e238cfcf802203244a286c0f40a4a
rmpestano <rmpestano@gmail.com>
2019-04-06 11:45:32 -0300

    adds k8s deployment

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,2 @@
-FROM rmpestano/wildfly:16.0.0
+FROM rmpestano/wildfly:16.0.1
 COPY ./target/cdi-crud.war ${DEPLOYMENT_DIR}
--- build-and-run.sh
+++ build-and-run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-mvn clean package && docker build -t rmpestano/cdi-crud . && docker run -it --rm --name cdi-crud -p 8080:8080 rmpestano/cdi-crud
+mvn clean package && docker build -t rmpestano/cdi-crud . && docker run -it --rm --name cdi-crud -p 8080:8080 -p 9990:9990 rmpestano/cdi-crud
